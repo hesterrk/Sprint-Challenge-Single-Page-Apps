30d36f1b72abb590c2e04f0304efbb6f7a59bb2a
Hester Knight <hesterknight@Hesters-MacBook-Pro.local>
2019-11-15 09:15:10 +0000

    fetched from axios

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,10 @@
 import React from "react";
+import axios from axios;
+
 
 export default function CharacterCard() {
+
+
+    
   return <span>todo: character</span>;
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,47 @@
 import React, { useEffect, useState } from "react";
 
-export default function CharacterList() {
+export default function CharacterList(props) {
   // TODO: Add useState to track data from useEffect
 
+  const [character, setCharacter] = useState([])
+
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    const getCharacters = () => {
+      axios
+      .get('https://rickandmortyapi.com/api/character/')
+      .then(response => {
+        getCharacters(response.data);
+
+    })
+
+    .catch(error => {
+      console.error('Server Error', error);
+    });
+}
+
+getCharacters();
+}, []);
+
+
 
   return (
     <section className="character-list">
       <h2>TODO: `array.map()` over your state here!</h2>
     </section>
+  );
+}
+
+
+ 
+     
+          
+        
+  
+  return (
+    <div className="movie-list">
+      {movies.map(movie => (
+        <MovieDetails key={movie.id} movie={movie} />
+      ))}
+    </div>
   );
 }
