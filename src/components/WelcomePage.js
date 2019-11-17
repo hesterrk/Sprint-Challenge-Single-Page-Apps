9e9316fd0e386d0a837e64d244551af685fc90de
Hester Knight <hesterknight@Hesters-MacBook-Pro.local>
2019-11-17 13:03:58 +0000

    styled section done

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,16 +1,28 @@
 import React from "react";
+import Styled from "styled-components";
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
-      <header>
+      <Header>
         <h1>Welcome to the ultimate fan site!</h1>
         <img
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
         />
-      </header>
+      </Header>
     </section>
   );
 }
+
+const Header = Styled.header`
+display: flex;
+justify-content: center;
+flex-direction: column;
+max-width: 500px;
+margin: 0 auto;
+
+
+
+`;
