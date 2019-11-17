0fd514e4199443898e8c268e0a8a0a7c0c964b1a
Hester Knight <hesterknight@Hesters-MacBook-Pro.local>
2019-11-17 16:37:19 +0000

    added button

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,16 +1,18 @@
 import React from "react";
 import Styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
       <Header>
         <h1>Welcome to the ultimate fan site!</h1>
-        <img
+         <img
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
         />
+        <Link to='/characters'><button>Characters</button></Link>
       </Header>
     </section>
   );
