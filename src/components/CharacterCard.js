a593b64fdb76b767a7b27d929ebb99f8fad88596
Hester Knight <hesterknight@Hesters-MacBook-Pro.local>
2019-11-17 14:41:59 +0000

    given props for each indivdual character using map index

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,6 +1,15 @@
 import React from "react";
+import Styled from 'styled-components';
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+  <div>
+  <div>{props.data.name}</div>
+  <div><img src={props.data.image}/></div>
+  <div>{props.data.status}</div>
+
+</div>
+  )
 }
+
 
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -29,10 +29,16 @@ export default function CharacterList() {
   return (
     <section className="character-list">
       <h2>{character.map(card => {
-        return <CharacterCard />
+        return <CharacterCard data={card} />
       }
-      
+
       )}</h2>
     </section>
   );
 }
+
+//saved returning data from axios into state
+//map over and we make it return the charactercard component- that will take a sliece of information by passing props "card"
+//the card in our map loops over 1st, 2nd etc.. 
+//card is a variable as it changes(we put it in curely because of this)- 1st is 1st eleemnt, then second the third eleemtn//
+//whatever is in card- give me whats in it
